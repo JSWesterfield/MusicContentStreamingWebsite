6953e1f33cec299ac08a68f1b81401b20d7d6d47
JSWesterfield <jecobwesterfield@gmail.com>
2018-01-28 01:16:44 -0700

    better ajax function to create weatherData object for placing response data to the css selector in our weather app template, as well as create afunction to round the temperature response data to 2 digits. Also we added a cool function to switchUnits from celsius back to Fahrenheit with a if else statement, then we ste the temperature value with the appropriate weatherData inside the DOM directly using the innerHTML function. But the jest of the weather app is it grabs the IP data from the ip-api API sets the regionName or the state to the this location response data, and then passes in the location response to the a variable called locationData, then we set the DarkSkyAPI to a variable, as well as set the unit to SI, instead of metric, and set our darkSKyAPI key to a regualr variable as well. Lastly we place the a corsProxy to bypass the CORS/cross domain error well recieve from passing in ip-api data to darkskiAPI. We throw all these new variable into one giant requestObject variable that we pass into our get method ajax function, and set the res to be set to certain variables well set to dom elements using our previously create weatherData object and their use of the

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,36 +1,71 @@
-// get geolocation data from ip via ip-api.com
-var getIP = 'https://ip-api.com/json/';
-$.getJSON(getIP).done(function(location) {
-    //show location object in console, to pull out 'regionName' or the state for rendering in html
-    console.log(location);
-    console.log("Your region/state name is: " + location.regionName);
-      var regionName = location.regionName;
-      var cityName = location.city;
-      // Now that we have the location data we can pass this within our Dark Sky Ajax call
-      var darkSkyURL = 'https://'
-      // To bypass the Cross Domain Error, can I pass in an object of mode: 'no-cors' as my proxy variable?
-      var corsProxy = 'https://cors-anywhere.herokuapp.com/' //can I pass in a mode: 'no-cors' object in place of this proxy?
-      var apiKey = '845ff6b003aba8bd1a9434ab040ef79c/' //darkSkyAPI key
-      var locationData = location.lat + "," + location.lon; 
-      var requestObject = corsProxy + darkSkyURL + apiKey + locationData;
-      // start Ajax call pass in the above data, in order:
-      // CORS + DarkSky API URL + API KEY + locationData
-      $.getJSON(darkSkyURL).done(function(response) {
-            console.log(response);
-      }, false);
+var weatherData = {
+    city: document.querySelector ("#userCity"),
+    state: document.querySelector ("#userState"), 
+    weather: document.querySelector ("#userWeather"),
+    temperature: document.querySelector("#userTemperature"),
+    icon: document.querySelector ('#userWeatherIcon'),
+    temperatureValue: 0,
+    units: "°F"
+  };
 
-        //create a weatherObject template/model for us to pass our response weather data into
-        const weatherObject = {
-            weather: response.weather,
-            temperature: response.temperature,
-            city: location.cityName,
-            state: location.regionName
-        };
+  // Might want to refactor code to make this a global function
+  // so that multiple 
+  // var roundTemperature = function(temperature) {
+  //       temperature = temperature.toFixed(1);
+  //       return temparature;
+  // }
+  function roundTemperature(temperature){
+        temperature = temperature.toFixed(1);
+        return temperature;
+  }
 
-        //Set selectors within my .ejs view to use the response data, will overright current html
-        $('#weather').html(weatherObject.weather);
-        $('#temperature').html(weatherObject.temperature);
-        $('#city').html(weatherObject.city);
-        $('#state').html(weatherObject.state);
+  function switchUnits () {
+    if (weatherData.units == "°C") {
+      weatherData.temperatureValue = roundTemperature(weatherData.temperatureValue * 9/5 + 32);
+      weatherData.units = "°F";
+    
+    } else {
+      weatherData.temperatureValue = roundTemperature ((weatherData.temperatureValue - 32) * 5/9);
+        weatherData.units = "°C";  
+    }
+      weatherData.temperature.innerHTML = weatherData.temperatureValue + weatherData.units + " ";
+  }
+  //https://api.darksky.net/forecast/845ff6b003aba8bd1a9434ab040ef79c/37.8267,-122.4233
+  // get geolocation data from ip via ip-api.com
+  var getIP = 'http://ip-api.com/json/';
+  //Dark Sky API key: 845ff6b003aba8bd1a9434ab040ef79c 
+  $.getJSON(getIP).done(function(location) {
+      //show location object in console, to pull out 'regionName' or the state for rendering in html
+      console.log(location);
+      console.log("Your region/state name is: " + location.regionName);
+        var regionName = location.regionName;
+        weatherData.state.innerHTML = regionName + ", ";
+        var cityName = location.city;
+        //Now that we have the location data we can pass this within our DarkSky Ajax call
+        var darkSkyURL = 'https://api.darksky.net/forecast/'
+        // To bypass the Cross Domain Error, can I pass in an object of mode: 'no-cors' as my proxy variable?
+        var corsProxy = 'https://cors-anywhere.herokuapp.com/' //can I pass in a mode: 'no-cors' object in place of this proxy?
+        var apiKey = '845ff6b003aba8bd1a9434ab040ef79c/' 
+        var units = '/?units=si'
+        var locationData = location.lat + "," + location.lon; 
+        var requestObject = corsProxy + darkSkyURL + apiKey + locationData + units;
+        //start Ajax call pass in the above data, in order:
+        // CORS + DarkSky API URL + API KEY + locationData
+        $.getJSON(requestObject).done(function(response) {
+              console.log(response);
+              var weatherDescription = response.currently.summary;
+              //set our initial temperature to Fahrenheit due to the response data return the unit of celsius as the response data
+              var weatherTemperature = (roundTemperature((response.currently.temperature) * 9/5 + 32));
 
-});  
+              var userWeatherIcon = response.currently.icon;
+              /* Set icons to the value of specific Skycons functions*/
+              var icons = new Skycons({"color": "white"});
+              icons.add(document.getElementById('userWeatherIcon'), userWeatherIcon);
+              icons.play();
+              
+              weatherData.temperatureValue = weatherTemperature;
+              weatherData.city.innerHTML = cityName;
+              weatherData.temperature.innerHTML = weatherTemperature + weatherData.units;
+        }, false);
+  });  
+
